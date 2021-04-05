df7f32e6640e808784c881cfbd86e805599ea744
huhaibo01 <padding4me@gmail.com>
2021-04-05 17:18:26 +0800

    feat: add dist

---
+++ dist/precache-manifest.1eae15dc6f3b4d4425e73535a887ce4c.js
@@ -0,0 +1,150 @@
+self.__precacheManifest = [
+  {
+    "revision": "b82244dc157413c3a727",
+    "url": "/css/app.bfe76367.css"
+  },
+  {
+    "revision": "b82244dc157413c3a727",
+    "url": "/js/app.435fb7a0.js"
+  },
+  {
+    "revision": "a6896d188e595eae4c3d",
+    "url": "/css/chunk-4e0b8328.1fead564.css"
+  },
+  {
+    "revision": "a6896d188e595eae4c3d",
+    "url": "/js/chunk-4e0b8328.4caaf817.js"
+  },
+  {
+    "revision": "8bea5915dddf5ee4be14",
+    "url": "/js/chunk-52e4de86.261592fc.js"
+  },
+  {
+    "revision": "b8c0f5cba2eb94871178",
+    "url": "/js/chunk-vendors.1b66c761.js"
+  },
+  {
+    "revision": "c1733565b32b585676302d4233c39da8",
+    "url": "/fonts/nucleo-icons.c1733565.eot"
+  },
+  {
+    "revision": "2569aaea6eaaf8cd210db7f2fa016743",
+    "url": "/fonts/nucleo-icons.2569aaea.woff"
+  },
+  {
+    "revision": "426439788ec5ba772cdf94057f6f4659",
+    "url": "/fonts/nucleo-icons.42643978.woff2"
+  },
+  {
+    "revision": "f82ec6ba2dc4181db2af35c499462840",
+    "url": "/fonts/nucleo-icons.f82ec6ba.ttf"
+  },
+  {
+    "revision": "0b8a30b10cbe7708d5f3a4b007c1d665",
+    "url": "/img/nucleo-icons.0b8a30b1.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/fonts/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/fonts/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/fonts/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/fonts/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/img/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "d06120a81f745a2bfe9522e3259e3b5f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "afc8c69fe2739cee7ed3f8d7db751de0",
+    "url": "/img/brand/alaya.png"
+  },
+  {
+    "revision": "185288d13ed8e9d745bd279ea34667bf",
+    "url": "/img/brand/blue.png"
+  },
+  {
+    "revision": "b9949387c6179e2dc4c675134a7b7935",
+    "url": "/favicon.png"
+  },
+  {
+    "revision": "8e55eab46b5fcfc4a7a0b27cb07c8888",
+    "url": "/img/brand/github-white-slim.png"
+  },
+  {
+    "revision": "c85c75275c0a0a617f9e5accc2700908",
+    "url": "/img/brand/creativetim-white-slim.png"
+  },
+  {
+    "revision": "b9949387c6179e2dc4c675134a7b7935",
+    "url": "/img/brand/favicon.png"
+  },
+  {
+    "revision": "bd527dafd82f3080e6c0c1435a3e6df6",
+    "url": "/img/brand/platon.jpeg"
+  },
+  {
+    "revision": "5967a9b0e4bb7ccbbacb1f3cf13cc848",
+    "url": "/img/brand/fibos.jpeg"
+  },
+  {
+    "revision": "10ef03a4140753cf4c29eab7057b38ee",
+    "url": "/img/brand/logo-purple.png"
+  },
+  {
+    "revision": "d4a76f2beb062344deeb4110e7efab0b",
+    "url": "/img/brand/white.png"
+  },
+  {
+    "revision": "594b1ee1d95ada356eaad078e9217932",
+    "url": "/img/ill/ill-2.svg"
+  },
+  {
+    "revision": "fd4a34d026fb9e0f4867188d47b11ba8",
+    "url": "/img/theme/img-1-1200x1000.jpg"
+  },
+  {
+    "revision": "7789b5bfa57722dd8916b1b9ff1b1d37",
+    "url": "/img/theme/img-2-1200x1000.jpg"
+  },
+  {
+    "revision": "dc49ad52655e1d9d0552c026db3ef688",
+    "url": "/img/theme/landing.jpg"
+  },
+  {
+    "revision": "974088a1931e40895bac6db119c62448",
+    "url": "/img/theme/promo-1.png"
+  },
+  {
+    "revision": "20d702b83a06bdb2ea71c4c0cb9a7a56",
+    "url": "/img/theme/profile.jpg"
+  },
+  {
+    "revision": "edc7106b21ec12e57022b2ebd534cd2d",
+    "url": "/img/theme/team-1-800x800.jpg"
+  },
+  {
+    "revision": "be997d5226b992ffad34816870c6b7aa",
+    "url": "/img/theme/team-2-800x800.jpg"
+  },
+  {
+    "revision": "54e3f3c414bd8e7234bae3ee3be950e5",
+    "url": "/img/theme/team-3-800x800.jpg"
+  },
+  {
+    "revision": "66618a418175ddf2ac8c47a241d327a8",
+    "url": "/img/theme/team-4-800x800.jpg"
+  }
+];
